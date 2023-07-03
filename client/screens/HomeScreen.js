eb05173344565008b590f74250196ba36f1a39bc
Phahladira-Moja <phahladira@patala.co.za>
2023-07-03 12:03:00 +0200

    Added the dummy constants data and Categories Component

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,10 +1,47 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, TextInput, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
+import * as Icon from "react-native-feather";
+import { themeColors } from "../theme";
+import Categories from "../components/Categories";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView className="bg-white">
+      <StatusBar barStyle="dark-content" />
+
+      {/* search bar */}
+      <View className="flex-row items-center space-x-2 px-4 pb-2">
+        <View className="flex-row flex-1 items-center p-3 rounded-full border border-gray-300">
+          <Icon.Search height="25" width="25" stroke="gray" />
+          <TextInput placeholder="Restaurants" className="ml-2 flex-1" />
+          <View className="flex-row items-center space-x-1 border-0 border-l-2 p-2 border-l-gray-300">
+            <Icon.MapPin height="20" width="20" stroke="gray" />
+            <Text className="text-gray-600"> New York, NYC </Text>
+          </View>
+        </View>
+
+        <View
+          className="p-3 rounded-full"
+          style={{ backgroundColor: themeColors.bgColor(1) }}
+        >
+          <Icon.Sliders
+            height="20"
+            width="20"
+            strokeWidth={2.5}
+            stroke="white"
+          />
+        </View>
+      </View>
+
+      {/* main  */}
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 20 }}
+      >
+        <Categories />
+      </ScrollView>
     </SafeAreaView>
   );
 };
