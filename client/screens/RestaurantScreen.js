74369c340bb7f85e041f73e94390040cf614fab5
Phahladira-Moja <phahladira@patala.co.za>
2023-07-03 11:16:40 +0200

    Created client side of application
    - Added all relevant packages

---
+++ client/screens/RestaurantScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+
+const RestaurantScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>RestaurantScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default RestaurantScreen;
